bee6ebea966c222fe48d2369485336a84f0969ef
sadra1ne <74346082+sadra1ne@users.noreply.github.com>
2020-12-08 21:24:33 +0330

    Add files via upload

---
+++ commands/stop.js
@@ -0,0 +1,16 @@
+const { canModifyQueue } = require("../util/EvobotUtil");
+
+module.exports = {
+  name: "stop",
+  description: "آهنک رو قطع می کند",
+  execute(message) {
+    const queue = message.client.queue.get(message.guild.id);
+
+    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!canModifyQueue(message.member)) return;
+
+    queue.songs = [];
+    queue.connection.dispatcher.end();
+    queue.textChannel.send(`${message.author} ⏹ آهنگ متوفق شد!`).catch(console.error);
+  }
+};
